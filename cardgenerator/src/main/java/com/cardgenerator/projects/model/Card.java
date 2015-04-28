d1689271f78c560173aee3814a76a75bd9f777ab
Juan Gomez <erioly@gmail.com>
2015-04-28 00:23:07 -0400

    Create Card model

---
+++ cardgenerator/src/main/java/com/cardgenerator/projects/model/Card.java
@@ -0,0 +1,21 @@
+package com.cardgenerator.projects.model;
+
+public abstract class Card {
+	
+	private String title;
+	private String description;
+	
+	public Card(String title, String description) {
+		
+		this.title = title;
+		this.description = description;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+}
